7301ea2954ffd27e6c8d31eeb78266a4500b9c1b
AmiralBl3ndic <camille.briand@efrei.net>
2020-11-30 19:32:30 +0100

    refactor(Continent): move Continent to its own file

--- src/main/scala/fr/efrei/fp/model/Country.scala
+++ src/main/scala/fr/efrei/fp/model/Country.scala
@@ -4,27 +4,7 @@ import fr.efrei.fp.util.Digit
 
 case class Country(id: Array[Digit], code: String, name: String, continent: Continent, wikipedia_link: String, keywords: Array[String])
 
-sealed trait Continent
-object Continent {
-  case object EU extends Continent
-  case object AS extends Continent
-  case object NA extends Continent
-  case object AF extends Continent
-  case object SA extends Continent
-  case object OC extends Continent
-  case object AN extends Continent
 
-  def parse(s: String): Either[String, Continent] = s match {
-    case "EU" => Right(Continent.EU)
-    case "AS" => Right(Continent.AS)
-    case "NA" => Right(Continent.NA)
-    case "AF" => Right(Continent.AF)
-    case "SA" => Right(Continent.SA)
-    case "OC" => Right(Continent.OC)
-    case "AN" => Right(Continent.AN)
-    case _ => Left(s"Unable to parse Continent from \"$s\"")
-  }
-}
 
 object Country {
   private def parseCountryId(countryId: String): Either[String, Array[Digit]] = {
