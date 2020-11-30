a661d6ca1c8ebb8b5ac946ff871b3655c4819947
AmiralBl3ndic <camille.briand@efrei.net>
2020-11-30 20:14:30 +0100

    feat(Country): add country name special characters checking No characters set for the moment, they still remain to define.

--- src/main/scala/fr/efrei/fp/model/Country.scala
+++ src/main/scala/fr/efrei/fp/model/Country.scala
@@ -1,14 +1,15 @@
 package fr.efrei.fp.model
 
 import fr.efrei.fp.util.Digit
-
 import java.net.URL
-import scala.util.Try
 
+import scala.util.Try
 
 case class Country(id: Array[Digit], code: String, name: String, continent: Continent, wikipedia_link: URL, keywords: Array[String])
 
 object Country {
+  private val nonCountryCharRegex = """[_]""".r
+
   private def parseId(countryId: String): Either[String, Array[Digit]] = {
     val countryIdDigits = countryId.toCharArray.map(Digit.buildFrom)
 
@@ -25,6 +26,11 @@ object Country {
     }
   }
 
+  private def parseName(name: String): Either[String, String] = name match {
+    case nonCountryCharRegex(_*) => Right(name)
+    case _ => Left(s"\"$name\" contains unauthorized characters")
+  }
+
   def buildFrom(csvLine: String): Either[String, Country] = {
     val csvBits = csvLine.split(",").map(
       _.trim
@@ -32,23 +38,23 @@ object Country {
 
     csvBits.length match {
       case x if x < 6 => Left("Unable to parse incomplete data, 6 columns expected, only $x found")
-      case _ => {
+      case _ =>
         // Parse id and ensure it is only made of digits
         val idEither = parseId(csvBits(0))
         val countryCode = csvBits(1)
-        val countryName = csvBits(2)
+        val nameEither = parseName(csvBits(2))
         val continentEither = Continent.parse(csvBits(3))
         val wikiLinkEither = Try(new URL(csvBits(4))).toEither
         val keywords = csvBits.drop(5)
 
-        (idEither, continentEither, wikiLinkEither) match {
-          case (Left(_), _, _) => Left("Unable to parse country ID")
-          case (_, Left(_), _) => Left("Unable to parse continent")
-          case (_, _, Left(_)) => Left("Unable to parse Wiki link")
-          case (Right(id), Right(continent), Right(wikiLink)) => Right(Country(id, countryCode, countryName, continent, wikiLink, keywords))
+        (idEither, nameEither, continentEither, wikiLinkEither) match {
+          case (Left(x), _, _, _) => Left(x)
+          case (_, Left(x), _, _) => Left(x)
+          case (_, _, Left(x), _) => Left(x)
+          case (_, _, _, Left(_)) => Left("Unable to parse wikipedia link")
+          case (Right(id), Right(name), Right(continent), Right(wikiLink)) => Right(Country(id, countryCode, name, continent, wikiLink, keywords))
           case _ => Left("Unable to parse country for some reason")
         }
-      }
     }
   }
 }
