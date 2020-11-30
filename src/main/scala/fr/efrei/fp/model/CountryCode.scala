85c3a9202b63f414a3b0058f2555eccd03b37e11
AmiralBl3ndic <camille.briand@efrei.net>
2020-11-30 20:27:47 +0100

    feat(CountryCode): add CountryCode sealed trait and object

---
+++ src/main/scala/fr/efrei/fp/model/CountryCode.scala
@@ -0,0 +1,13 @@
+package fr.efrei.fp.model
+
+sealed trait CountryCode
+object CountryCode {
+  private val countryCodeRegex = """^[A-Z]{2}$""".r
+
+  def parse(code: String): Either[String, CountryCode] = code match {
+    case countryCodeRegex(_*) => Right(CountryCodeImpl(code))
+    case _ => Left(s"\"$code\" is not a valid country code")
+  }
+
+  private case class CountryCodeImpl(code: String) extends CountryCode
+}
