ec5eb866192cca578a257faf9dc4d76dbabd6ba8
Toto94463 <36296512+Toto94463@users.noreply.github.com>
2020-12-03 14:56:07 +0100

    Update Airport.scala

--- src/main/scala/fr/efrei/fp/model/Airport.scala
+++ src/main/scala/fr/efrei/fp/model/Airport.scala
@@ -1,12 +1,29 @@
 package fr.efrei.fp.model
 
-case class Airport( name: String,
-	                  pays: String,
-	                  ville:String,
-	                  destination:List[String])
+case class Airport( id: int,
+                    ident: String,
+                    types: String,
+                    name: String,
+                    latitude_deg: float,
+                    longitude_deg: float,
+                    elevation_ft: int,
+                    continent: String,
+                    iso_country: String,
+                    iso_region: String,
+                    municipality: String,
+                    scheduled_service: boolean,
+                    gps_code: String,
+                    iata_code: String,
+                    lacal_code: String,
+                    home_link: String,
+                    wikipedia_link: String,
+                    keywords: String,
+                  )
 
 object Airport {
-	def build(name: String, pays: String, ville:String, destination:List[String]): Option[Airport] =
-		if (pays=="test") Some(new Airport(name,pays,ville,destination))
+	def build(id: int,ident: String,types: String,name: String,latitude_deg: float,longitude_deg: float,elevation_ft: int,continent: String,iso_country: String,iso_region: String,
+            municipality: String,scheduled_service: boolean,gps_code: String,iata_code: String,lacal_code: String,home_link: String,wikipedia_link: String,keywords: String,):
+            Option[Airport] =
+		if (id=="test") Some(new Airport(name,pays,ville,destination))
 		else None
 }
