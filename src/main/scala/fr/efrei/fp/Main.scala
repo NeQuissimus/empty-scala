3860fbca53cbec76e44a8a0f5bdabbc943985504
AmiralBl3ndic <camille.briand@efrei.net>
2021-01-05 11:05:59 +0100

    refactor(Main): make CSV parsing happen in chain

--- src/main/scala/fr/efrei/fp/Main.scala
+++ src/main/scala/fr/efrei/fp/Main.scala
@@ -3,18 +3,26 @@ package fr.efrei.fp
 import fr.efrei.fp.model.{Airport, Country, Runway}
 
 object Main extends App {
+  println("Parsing CSV files...")
+
 	Country.parseAllFromCSV("./src/resources/countries.csv") match {
     case Left(x) => println(x)
-    case Right(countries) => countries.foreach(println)
-  }
+    case Right(countries) =>
+      println("Done parsing and gathering parsable countries from countries.csv")
+      countries.foreach(println)
 
-  Airport.parseAllFromCSV("./src/resources/airports.csv") match {
-    case Left(x) => println(x)
-    case Right(airports) => airports.foreach(println)
-  }
+      Airport.parseAllFromCSV("./src/resources/airports.csv") match {
+        case Left(x) => println(x)
+        case Right(airports) =>
+          println("Done parsing and gathering parsable airports from airports.csv")
+          airports.foreach(println)
 
-  Runway.parseAllFromCSV("./src/resources/runways.csv") match {
-    case Left(x) => println(x)
-    case Right(runways) => runways.foreach(println)
+          Runway.parseAllFromCSV("./src/resources/runways.csv") match {
+            case Left(x) => println(x)
+            case Right(runways) =>
+              println("Done parsing and gathering parsable runways from runways.csv")
+              runways.foreach(println)
+          }
+      }
   }
 }
